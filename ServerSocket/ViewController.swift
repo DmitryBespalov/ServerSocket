3182ebc0bd5727e68c9d56bae835539aeaf2e434
diamondsky <dima.bespalov@gmail.com>
2017-03-01 18:58:37 +0100

    Initial Commit

---
+++ ServerSocket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ServerSocket
+//
+//  Created by Dmitry Bespalov on 01/03/17.
+//  Copyright © 2017 Dmitry Bespalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
